a9e2fef7b68d1c5994ebbc6cadb1f00688a2c523
rikich3 <rchambillap@unsa.edu.pe>
2024-06-01 15:00:43 -0500

    Terminado todas las implementaciones

--- lab5/picture.py
+++ lab5/picture.py
@@ -16,43 +16,92 @@ class Picture:
     vertical = []
     for value in self.img:
       vertical.append(value[::-1])
-    return vertical
+    return Picture(vertical)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
     horizontal = []
     for value in self.img[::-1]:
       horizontal.append(value)
-    return Picture(None)
+    return Picture(horizontal)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
-    return Picture(None)
+    newPic = []
+    str = ""
+    for s in self.img:
+      str = ""
+      for c in s:
+        str = str + self._invColor(c)
+      newPic.append(str)
+    return Picture(newPic)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    return Picture(None)
+    joined = []
+    for c in range(0, len(self.img)):
+      joined.append(self.img[c] + p.img[c])
+    return Picture(joined)
 
   def up(self, p):
-    return Picture(None)
+    newp = self.img + p.img
+    return Picture(newp)
 
   def under(self, p):
     """ Devuelve una nueva figura poniendo la figura p sobre la
         figura actual """
-    return Picture(None)
+    newp = p.img + self.img
+    return Picture(newp)
   
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
+    p = []
+    for c in range(0, len(self.img)):
+      p.append("")
+    pic = Picture(p)
+    for i in range(0, n):
+      pic = pic.join(self)
+    return pic
 
   def verticalRepeat(self, n):
-    return Picture(None)
+    if n is 0:
+      return Picture(None)
+    pic = Picture(self.img)
+    for i in range(0, n-1):
+      pic = pic.under(self)
+    return Picture(pic)
 
   #Extra: Sólo para realmente viciosos 
-  def rotate(self):
+  def rotateClock(self):
     """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
     o antihorario"""
-    return Picture(None)
+    """Para realizar esto nos fijamos que todas las columnas pasaran a ser ser filas
+      Por lo que hacemos un for en el que agarramos todos los primeros characteres y los colocamos en string nesimo
+      por seguridad tendre que realizar hasta el tamaño del primer string, luego vere para que agarre el mayor y rellene
+      con blanco cuando se encuentre con errores de indexacion"""
+    rotated = []
+    s_ = ""
+    for i in range(0, self._getWidth()):
+      for j in range(self._getHeight()):
+        try:
+          s_ += self.img[self._getHeight()- (j - 1)][i]
+        except:
+          s_ += " "
+      rotated.append(s_)
+      s_= ""
+    return Picture(rotated)
+  def rotateAntiClock(self):
+    """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
+    o antihorario"""
+    return self.rotateClock().rotateClock().rotateClock()
 
+  def _getHeight(self):
+    return len(self.img)
+  def _getWidth(self):
+    max = 0
+    for i in self.img:
+      if len(i) > max:
+        max = len(i)
+    return max
